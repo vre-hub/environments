2ad5b56ba5093e2319fe2eb73bbe0af609a2f970
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-07-03 11:01:16 +0200

    fix permissions of .jupyter dir

--- vre-singleuser-dev/setup.sh
+++ vre-singleuser-dev/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 python /opt/setup-rucio-jupyterlab/configure.py
-echo -e "c.ServerApp.max_body_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
-echo -e "c.ServerApp.max_buffer_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
+# echo -e "c.ServerApp.max_body_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
+# echo -e "c.ServerApp.max_buffer_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
 exec "$@"
