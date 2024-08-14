cf69c9301f5bf8d627ead11dadd602ee3dbb5137
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-08-14 13:43:59 +0200

    remove exec

--- vre-singleuser-dev/setup-vre.sh
+++ vre-singleuser-dev/setup-vre.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -e
 python /usr/local/bin/configure-vre.py
-# echo -e "c.ServerApp.max_body_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
-# echo -e "c.ServerApp.max_buffer_size = 2100100100" >> /etc/jupyter/jupyter_server_config.py
-exec "$@"
