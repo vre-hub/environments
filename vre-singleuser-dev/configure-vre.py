5f3e81fadba4cdf340a34b03b9ab2d038f0c49c8
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-10-08 11:03:26 +0200

    build dev image accessing atlas RSE

--- vre-singleuser-dev/configure-vre.py
+++ vre-singleuser-dev/configure-vre.py
@@ -4,12 +4,12 @@
 import os
 import json
 
-HOME =  os.getenv('HOME')
+HOME = '/home/jovyan'
 
 def write_jupyterlab_config():
-    file_path = HOME + '/.jupyter/jupyter_notebook_config.json'
+    file_path = HOME + '/.jupyter/jupyter_server_config.json'
     if not os.path.isfile(file_path):
-        os.makedirs(HOME +'/.jupyter', exist_ok=True)
+        os.makedirs(HOME + '/.jupyter/', exist_ok=True)
     else:
         config_file = open(file_path, 'r')
         config_payload = config_file.read()
@@ -24,7 +24,7 @@ def write_jupyterlab_config():
 #  either ("destination_rse", "rse_mount_path") either ("rucio_ca_cert") are required env vars, even if they are defined in the jhub manifest.
 # Adding 'rucio_base_url' too - from debugging experience
 
-    instance_config = {
+    escape_config = {
         "name": os.getenv('RUCIO_NAME', 'default'),
         "display_name": os.getenv('RUCIO_DISPLAY_NAME', 'Default Instance'),
         "rucio_base_url": os.getenv('RUCIO_BASE_URL', 'DEFAULT rucio base url'),
@@ -48,10 +48,35 @@ def write_jupyterlab_config():
         "oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME'),
     }
 
-    instance_config = {k: v for k,
-                       v in instance_config.items() if v is not None}
+    atlas_config = {
+        "name": 'atlas',
+        "display_name": 'ATLAS RUCIO',
+        "rucio_base_url": 'https://voatlasrucio-server-prod.cern.ch:443',
+        "rucio_auth_url": 'https://atlas-rucio-auth.cern.ch:443',
+        "rucio_webui_url": 'https://rucio-ui.cern.ch' ,
+        "rucio_ca_cert": os.getenv('RUCIO_CA_CERT'),
+        "site_name": os.getenv('RUCIO_SITE_NAME'),
+        "vo": 'atlas',
+        "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
+        "voms_vomses_path": os.getenv('RUCIO_VOMS_VOMSES_PATH'),
+        "voms_certdir_path": os.getenv('RUCIO_VOMS_CERTDIR_PATH'),
+        "voms_vomsdir_path": os.getenv('RUCIO_VOMS_VOMSDIR_PATH'),
+        "destination_rse": 'ATLAS-RUCIO',
+        "rse_mount_path": '/eos/atlas/atlasscratchdisk/rucio',
+        "path_begins_at": '4',
+        "mode": 'replica',
+        "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '1',
+        "oidc_auth": os.getenv('RUCIO_OIDC_AUTH', ''),
+        "oidc_env_name": os.getenv('RUCIO_OIDC_ENV_NAME', ''),
+        "oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME', ''),
+    }
+
+    escape_config = {k: v for k,
+                    v in escape_config.items() if v is not None}
+
+
     config_json['RucioConfig'] = {
-        'instances': [instance_config],
+        'instances': [escape_config, atlas_config],
         "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE'),
         "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE'),
     }
@@ -59,6 +84,38 @@ def write_jupyterlab_config():
     config_file = open(file_path, 'w')
     config_file.write(json.dumps(config_json, indent=2))
     config_file.close()
+
+def write_ipython_config():
+    file_path = HOME + '/.ipython/profile_default/ipython_kernel_config.json'
+    extension_module = 'rucio_jupyterlab.kernels.ipython'
+
+    if not os.path.isfile(file_path):
+        os.makedirs(HOME + '/.ipython/profile_default/', exist_ok=True)
+    else:
+        config_file = open(file_path, 'r')
+        config_payload = config_file.read()
+        config_file.close()
+
+    try:
+        config_json = json.loads(config_payload)
+    except:
+        config_json = {}
+
+    if 'IPKernelApp' not in config_json:
+        config_json['IPKernelApp'] = {}
+
+    ipkernel_app = config_json['IPKernelApp']
+
+    if 'extensions' not in ipkernel_app:
+        ipkernel_app['extensions'] = []
+
+    if extension_module not in ipkernel_app['extensions']:
+        ipkernel_app['extensions'].append(extension_module)
+
+    config_file = open(file_path, 'w')
+    config_file.write(json.dumps(config_json, indent=2))
+    config_file.close()    
     
 if __name__ == '__main__':
     write_jupyterlab_config()
+    write_ipython_config()
