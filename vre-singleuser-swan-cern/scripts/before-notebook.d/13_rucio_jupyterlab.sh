dce7bc087d80553cb9e1e531aad27b5c0fd82163
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-26 16:20:02 +0100

    not touch anything

--- vre-singleuser-swan-cern/scripts/before-notebook.d/13_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/13_rucio_jupyterlab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e 
-mkdir -p $HOME/.jupyter/
-# If file exists, erase it and create an empty one
-config_file="$HOME/.jupyter/jupyter_server_config.json"
-if [ -f "$config_file"] ; then rm -f $config_file ; fi
-sleep 1
-touch "$config_file"
+# mkdir -p $HOME/.jupyter/
+# # If file exists, erase it and create an empty one
+# config_file="$HOME/.jupyter/jupyter_server_config.json"
+# if [ -f "$config_file"] ; then rm -f $config_file ; fi
+# sleep 1
+# touch "$config_file"
 python /srv/singleuser/configure_rucio_jupyterlab.py
