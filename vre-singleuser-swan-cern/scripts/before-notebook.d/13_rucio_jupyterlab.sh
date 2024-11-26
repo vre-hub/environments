c79d4b74113fec1bcd0c1c590f9911f9bfcbdfd4
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-26 15:48:52 +0100

    typo in touch file again 2

--- vre-singleuser-swan-cern/scripts/before-notebook.d/13_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/13_rucio_jupyterlab.sh
@@ -3,6 +3,7 @@ set -e
 mkdir -p $HOME/.jupyter/
 # If file exists, erase it and create an empty one
 config_file="$HOME/.jupyter/jupyter_server_config.json"
-if [ -f "$file"] ; then rm -f $file ; fi
+if [ -f "$config_file"] ; then rm -f $config_file ; fi
+sleep 1
 touch "$config_file"
 python /srv/singleuser/configure_rucio_jupyterlab.py
