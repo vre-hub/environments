33c11319b45b46fa6ff812dea30db0b44b9eb8a7
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-29 18:26:14 +0100

    still hardcode - vive git

--- vre-singleuser-interTwin/configure.py
+++ vre-singleuser-interTwin/configure.py
@@ -28,33 +28,67 @@ def write_jupyterlab_config():
 #  either ("destination_rse", "rse_mount_path") either ("rucio_ca_cert") are required env vars, even if they are defined in the jhub manifest.
 # Adding 'rucio_base_url' too - from debugging experience
 
+    # instance_config = {
+    #     "name": os.getenv('RUCIO_NAME', 'default'),
+    #     "display_name": os.getenv('RUCIO_DISPLAY_NAME', 'Default Instance'),
+    #     "rucio_base_url": os.getenv('RUCIO_BASE_URL', 'DEFAULT rucio base url'),
+    #     "rucio_auth_url": os.getenv('RUCIO_AUTH_URL'),
+    #     "rucio_webui_url": os.getenv('RUCIO_WEBUI_URL'),
+    #     "rucio_ca_cert": os.getenv('RUCIO_CA_CERT', 'cacert.pem'),
+    #     "site_name": os.getenv('RUCIO_SITE_NAME'),
+    #     "vo": os.getenv('RUCIO_VO'),
+    #     "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
+    #     "voms_vomses_path": os.getenv('RUCIO_VOMS_VOMSES_PATH'),
+    #     "voms_certdir_path": os.getenv('RUCIO_VOMS_CERTDIR_PATH'),
+    #     "voms_vomsdir_path": os.getenv('RUCIO_VOMS_VOMSDIR_PATH'),
+    #     "destination_rse": os.getenv('RUCIO_DESTINATION_RSE', 'DEFAULT rse destination'),
+    #     "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH', 'DEFAULT rse mount path'),
+    #     "replication_rule_lifetime_days": int(os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS')) if os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS') else None,
+    #     "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
+    #     "mode": os.getenv('RUCIO_MODE', 'replica'),
+    #     "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '1',
+    #     "oidc_auth": os.getenv('RUCIO_OIDC_AUTH'),
+    #     "oidc_env_name": os.getenv('RUCIO_OIDC_ENV_NAME'),
+    #     "oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME'),
+    # }
+
+    # instance_config = {k: v for k,
+    #                    v in instance_config.items() if v is not None}
+    # config_json['RucioConfig'] = {
+    #     'instances': [instance_config],
+    #     "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE',),
+    #     "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE'),
+    # }
+
+    # Debugging and hardcoding from here
+
     instance_config = {
-        "name": os.getenv('RUCIO_NAME', 'rucio-intertwin-testbed.desy.de'),
-        "display_name": os.getenv('RUCIO_DISPLAY_NAME', 'interTwinRUCIO'),
-        "rucio_base_url": os.getenv('RUCIO_BASE_URL', 'https://rucio-intertwin-testbed.desy.de'),
-        "rucio_auth_url": os.getenv('RUCIO_AUTH_URL', 'https://rucio-intertwin-testbed-auth.desy.de'),
-        "rucio_webui_url": os.getenv('RUCIO_WEBUI_URL'),
-        "rucio_ca_cert": os.getenv('RUCIO_CA_CERT', '/opt/conda/lib/python3.9/site-packages/certifi/cacert.pem'),
-        "site_name": os.getenv('RUCIO_SITE_NAME', 'VEGA'),
-        "vo": os.getenv('RUCIO_VO'),
+        "name": "rucio-intertwin-testbed.desy.de",
+        "display_name": "interTwinRUCIO",
+        "rucio_base_url": "https://rucio-intertwin-testbed.desy.de",
+        "rucio_auth_url": "https://rucio-intertwin-testbed-auth.desy.de",
+        "rucio_ca_cert": "/opt/conda/lib/python3.9/site-packages/certifi/cacert.pem",
+        "site_name": "VEGA",
         "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
-        "destination_rse": os.getenv('RUCIO_DESTINATION_RSE', 'VEGA-DCACHE'),
-        "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH'),
+        "destination_rse": "VEGA-DCACHE",
+        "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH', '/dcache/sling.si'),
         "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
-        "mode": "replica",  # "mode": os.getenv('RUCIO_MODE', 'replica'),
-        "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '1',
-        "oidc_auth": os.getenv('RUCIO_OIDC_AUTH', 'env'),
-        "oidc_env_name": os.getenv('RUCIO_OIDC_ENV_NAME', 'RUCIO_ACCESS_TOKEN'),
-        #"oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME'),
-    }
+        "mode": "replica", 
+        #"mode": "download", 
+        "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '0',
+        "oidc_auth": "env",
+        "oidc_env_name": "RUCIO_ACCESS_TOKEN"
+      }
 
     instance_config = {k: v for k,
                        v in instance_config.items() if v is not None}
     config_json['RucioConfig'] = {
         'instances': [instance_config],
-        "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE','rucio-intertwin-testbed.desy.de'),
+        "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE', 'rucio-intertwin-testbed.desy.de'),
         "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE', 'oidc'),
     }
+
+    # up to here
 
     config_file = open(file_path, 'w')
     config_file.write(json.dumps(config_json, indent=2))
@@ -67,7 +101,7 @@ def write_rucio_config():
     client_config = {
         'rucio_host': os.getenv('RUCIO_BASE_URL', 'https://rucio-intertwin-testbed.desy.de'),
         'auth_host': os.getenv('RUCIO_AUTH_URL', 'https://rucio-intertwin-testbed-auth.desy.de'),
-        'ca_cert': os.getenv('RUCIO_CA_CERT', '/certs/rucio_ca.pem'),  # /opt/conda/lib/python3.9/site-packages/certifi/cacert.pem ?
+        'ca_cert': os.getenv('RUCIO_CA_CERT', '/certs/rucio_ca.pem'),
         'auth_type': os.getenv('RUCIO_AUTH_TYPE', 'oidc'), # 'x509' or 'oidc'
         'account': os.getenv('RUCIO_ACCOUNT', '$RUCIO_ACCOUNT'), # This is the RUCIO account name, need to be mapped from idp
         'oidc_polling': 'true',
