93d62fd8f873c1d79b597ee24ddd315ba40f36c0
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-12-13 11:09:03 +0100

    add both rucio instances

--- vre-singleuser-dev/configure-vre.py
+++ vre-singleuser-dev/configure-vre.py
@@ -24,29 +24,29 @@ def write_jupyterlab_config():
 #  either ("destination_rse", "rse_mount_path") either ("rucio_ca_cert") are required env vars, even if they are defined in the jhub manifest.
 # Adding 'rucio_base_url' too - from debugging experience
 
-    # escape_config = {
-    #     "name": os.getenv('RUCIO_NAME', 'default'),
-    #     "display_name": os.getenv('RUCIO_DISPLAY_NAME', 'Default Instance'),
-    #     "rucio_base_url": os.getenv('RUCIO_BASE_URL', 'DEFAULT rucio base url'),
-    #     "rucio_auth_url": os.getenv('RUCIO_AUTH_URL'),
-    #     "rucio_webui_url": os.getenv('RUCIO_WEBUI_URL'),
-    #     "rucio_ca_cert": os.getenv('RUCIO_CA_CERT'),
-    #     "site_name": os.getenv('RUCIO_SITE_NAME'),
-    #     "vo": os.getenv('RUCIO_VO'),
-    #     "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
-    #     "voms_vomses_path": os.getenv('RUCIO_VOMS_VOMSES_PATH'),
-    #     "voms_certdir_path": os.getenv('RUCIO_VOMS_CERTDIR_PATH'),
-    #     "voms_vomsdir_path": os.getenv('RUCIO_VOMS_VOMSDIR_PATH'),
-    #     "destination_rse": os.getenv('RUCIO_DESTINATION_RSE', 'DEFAULT rse destination'),
-    #     "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH', 'DEFAULT rse mount path'),
-    #     "replication_rule_lifetime_days": int(os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS')) if os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS') else None,
-    #     "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
-    #     "mode": os.getenv('RUCIO_MODE', 'replica'),
-    #     "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '1',
-    #     "oidc_auth": os.getenv('RUCIO_OIDC_AUTH'),
-    #     "oidc_env_name": os.getenv('RUCIO_OIDC_ENV_NAME'),
-    #     "oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME'),
-    # }
+    escape_config = {
+        "name": os.getenv('RUCIO_NAME', 'default'),
+        "display_name": os.getenv('RUCIO_DISPLAY_NAME', 'Default Instance'),
+        "rucio_base_url": os.getenv('RUCIO_BASE_URL', 'DEFAULT rucio base url'),
+        "rucio_auth_url": os.getenv('RUCIO_AUTH_URL'),
+        "rucio_webui_url": os.getenv('RUCIO_WEBUI_URL'),
+        "rucio_ca_cert": os.getenv('RUCIO_CA_CERT'),
+        "site_name": os.getenv('RUCIO_SITE_NAME'),
+        "vo": os.getenv('RUCIO_VO'),
+        "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
+        "voms_vomses_path": os.getenv('RUCIO_VOMS_VOMSES_PATH'),
+        "voms_certdir_path": os.getenv('RUCIO_VOMS_CERTDIR_PATH'),
+        "voms_vomsdir_path": os.getenv('RUCIO_VOMS_VOMSDIR_PATH'),
+        "destination_rse": os.getenv('RUCIO_DESTINATION_RSE', 'DEFAULT rse destination'),
+        "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH', 'DEFAULT rse mount path'),
+        "replication_rule_lifetime_days": int(os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS')) if os.getenv('RUCIO_REPLICATION_RULE_LIFETIME_DAYS') else None,
+        "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
+        "mode": os.getenv('RUCIO_MODE', 'replica'),
+        "wildcard_enabled": os.getenv('RUCIO_WILDCARD_ENABLED', '0') == '1',
+        "oidc_auth": os.getenv('RUCIO_OIDC_AUTH'),
+        "oidc_env_name": os.getenv('RUCIO_OIDC_ENV_NAME'),
+        "oidc_file_name": os.getenv('RUCIO_OIDC_FILE_NAME'),
+    }
 
     atlas_config = {
         "name": "https://voatlasrucio-server-prod.cern.ch",
@@ -65,17 +65,17 @@ def write_jupyterlab_config():
         
       }
 
-    # escape_config = {k: v for k,
-    #                 v in escape_config.items() if v is not None}
+    escape_config = {k: v for k,
+                    v in escape_config.items() if v is not None}
 
     atlas_config = {k: v for k,
                     v in atlas_config.items() if v is not None}
 
 
     config_json['RucioConfig'] = {
-        'instances': [atlas_config],
-        "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE'),
-        "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE'),
+        'instances': [escape_config, atlas_config],
+        "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE', escape_config['name']),
+        "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE', 'oidc'),
     }
 
     config_file = open(file_path, 'w')
