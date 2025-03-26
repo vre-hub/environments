f2152d184a5387cd52e59b29144eb3f7747e47a9
garciagenrique <enrique.garcia.garcia@cern.ch>
2025-03-26 18:03:40 +0100

    this will not trigger

--- vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
@@ -6,4 +6,5 @@ echo "id USER:"
 id $USER
 echo "pwd: "
 echo $PWD
-run_as_user python /srv/singleuser/configure_rucio_jupyterlab.py
+#run_as_user python /srv/singleuser/configure_rucio_jupyterlab.py
+sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
