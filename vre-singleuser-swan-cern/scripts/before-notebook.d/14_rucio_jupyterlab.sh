34eb198a2e6aef49aa8adaccf9a4fedf4b77a29a
garciagenrique <enrique.garcia.garcia@cern.ch>
2025-03-31 10:23:37 +0200

    try without before-notebook.d

--- vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
@@ -7,4 +7,4 @@ id $USER
 echo "pwd: "
 echo $PWD
 #run_as_user python /srv/singleuser/configure_rucio_jupyterlab.py
-sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
+#sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
