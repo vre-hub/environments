1454cd63255edfad5c541ac6b77200f2470129c9
garciagenrique <enrique.garcia.garcia@cern.ch>
2025-04-01 12:16:05 +0200

    run rucio_cfg via bash script

--- vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
@@ -6,6 +6,6 @@ echo "id USER:"
 id $USER
 echo "pwd: "
 echo $PWD
-python /srv/singleuser/configure_rucio_jupyterlab.py
+#python /srv/singleuser/configure_rucio_jupyterlab.py
 #run_as_user python /srv/singleuser/configure_rucio_jupyterlab.py
-#sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
+sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
