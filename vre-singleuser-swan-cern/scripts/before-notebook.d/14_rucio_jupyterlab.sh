3233dac9dfc0df5475a5d2b9c720f6a14d1075e0
garciagenrique <enrique.garcia.garcia@cern.ch>
2025-04-04 15:29:10 +0200

    try jupyter_server_config.py

--- vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
+++ vre-singleuser-swan-cern/scripts/before-notebook.d/14_rucio_jupyterlab.sh
@@ -8,4 +8,4 @@ echo "pwd: "
 echo $PWD
 #python /srv/singleuser/configure_rucio_jupyterlab.py
 #run_as_user python /srv/singleuser/configure_rucio_jupyterlab.py
-sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
+#sudo -u $NB_USER python /srv/singleuser/configure_rucio_jupyterlab.py
