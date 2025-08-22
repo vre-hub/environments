1a6beae3cf45062f4abce39787d28423bfbb3769
Giovanni Guerrieri <giovanni.guerrieri@cern.ch>
2025-08-22 18:47:45 +0200

    feat(ENV:vre-swan) execute rucio script first

--- vre-swan-cern/scripts/before-notebook.d/01_rucio.sh
+++ vre-swan-cern/scripts/before-notebook.d/01_rucio.sh
@@ -7,13 +7,13 @@
 
 # RUCIO_JUPYTERLAB_VERSION is set in the Dockerfile
 if [[ -z "$RUCIO_JUPYTERLAB_VERSION" ]]; then
-    echo "RUCIO_JUPYTERLAB_VERSION is not set. Please set it to the desired version."
+    _log "RUCIO_JUPYTERLAB_VERSION is not set. Please set it to the desired version."
     exit 1
 fi
 
 # Check if the Rucio JupyterLab version is available
 if [[ ! -d "/cvmfs/sw.escape.eu/rucio-jupyterlab/${RUCIO_JUPYTERLAB_VERSION}" ]]; then
-    echo "Rucio JupyterLab version ${RUCIO_JUPYTERLAB_VERSION} is not available."
+    _log "Rucio JupyterLab version ${RUCIO_JUPYTERLAB_VERSION} is not available."
     exit 1
 fi
 
@@ -27,17 +27,6 @@ export PATH="$RUCIO_HOME/bin:$PATH"
 export PYTHONPATH="${RUCIO_CVMFS_PATH}/lib/python3.11/site-packages:$PYTHONPATH"
 export RUCIO_CA_CERT=$RUCIO_HOME/etc/rucio_ca.pem
 export RUCIO_PYTHONBIN=python
-
-_log "PATH: $PATH"
-_log "PYTHONPATH: $PYTHONPATH"
-
-# Set PATH and PYTHONPATH as the user
-run_as_user "export PATH=\"$RUCIO_HOME/bin:\$PATH\""
-run_as_user "export PYTHONPATH=\"${RUCIO_CVMFS_PATH}/lib/python3.11/site-packages:\$PYTHONPATH\""
-
-# Log the values (run as user to see the actual user environment)
-run_as_user "echo \"PATH: \$PATH\""
-run_as_user "echo \"PYTHONPATH: \$PYTHONPATH\""
 
 _log "Rucio JupyterLab environment set up successfully."
 
