35b0352c34a56b0a6fc057b790ecb72ab1a9b66a
Giovanni Guerrieri <giovanni.guerrieri@cern.ch>
2025-10-22 10:05:18 +0200

    chore(ENV:vre-swan-cern) rename script and re-add paths

--- vre-swan-cern/scripts/before-notebook.d/13_rucio.sh
+++ vre-swan-cern/scripts/before-notebook.d/13_rucio.sh
@@ -35,20 +35,20 @@ export RUCIO_HOME="${RUCIO_CVMFS_PATH}"
 
 # Symlink all the Rucio binaries to the SWAN nb_term_bin directory.
 ln -s ${RUCIO_CVMFS_PATH}/bin/* $SWAN_BIN_DIR/nb_term_bin
-# export PATH="${RUCIO_CVMFS_PATH}/bin:${PATH}"
+export PATH="${RUCIO_CVMFS_PATH}/bin:${PATH}"
 
 # Pick up the jupyter server side python version from /opt/conda/bin/jupyter
 # Only the major.minor version is needed to select the correct site-packages path
 SERVER_PYTHON_VERSION=$(/opt/conda/bin/python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 echo "Using server python version: $SERVER_PYTHON_VERSION"
-ls -l ${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/site-packages/
+ls -l ${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/
 
 # Select the correct python version subdirectory, regardless of the patch version
 ln -s ${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/site-packages/* $SWAN_LIB_DIR/nb_term_lib
 
 # Add the Rucio JupyterLab Python packages directory to the PYTHONPATH environment variable.
 # export USER_PYTHONPATH="${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/site-packages"
-# export PYTHONPATH="${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/site-packages:${PYTHONPATH}"
+export PYTHONPATH="${RUCIO_CVMFS_PATH}/lib/python${SERVER_PYTHON_VERSION}/site-packages:${PYTHONPATH}"
 
 # --- End tests to setup paths ---
 
